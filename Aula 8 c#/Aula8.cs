0ca13c72578c783554cbed82332626149bc83366
Erika-Nunes <erikac0211@outlook.com>
2024-08-27 21:25:17 -0300

    Aula 8
    Arquivos aula 8 -  Senac

---
+++ Aula 8 c#/Aula8.cs
@@ -0,0 +1,73 @@
+//Aula 8 - 27/08/2024
+//Questão
+/*
+Classe Animal (a ser herdado)
+Funções:
+- Comer
+- FazerBarulho
+
+Classe Cachorro 
+Funções:
+- Comer
+- FazerBarulho
+
+Classe Gato
+Funções:
+- Comer
+- FazerBarulho
+
+Classe Pássaro
+Funções:
+- Comer
+- FazerBarulho
+
+
+-imprimirDetalhes
+------------------------------------
+
+Animal animal = new Animal();
+Cachorro c1 = new Cachorro();
+Gato g1 = new Gato();
+Passaro p1 = new Passaro();
+
+p1.Comer();
+p1.FazerBarulho();
+*/
+
+/*
+QUESTÃO
+Classe Calculadora 
+Atributos:
+- resultado 
+
+Funções:
+-GetResultado
+- Somar (numero)
+- Subtrair (numero)
+
+ex: Somar(10) - GetResultado() -- 10 - Subtrair(2) - GetResultado() -- 8
+
+---------------------------------------------
+
+Calculadora c = new Calculadora();
+
+Console.WriteLine(c.Getresultado());
+c.Somar(10);
+Console.WriteLine(c.Getresultado());
+c.Somar(50);
+Console.WriteLine(c.Getresultado());
+c.Subtrair(20);
+*/
+/*
+QUESTÃO
+Classe CarrinhoDeCompras
+Atributos:
+- vetor de string: nome dos produtos (adiciona aqui)
+- valor total (soma aqui) - tem que ser privado
+
+Funções:
+- Adicionar Item(nome, preço)
+- GetTotal
+- Imprimir a lista 
+*/
+//int[] vetor = new int[5];

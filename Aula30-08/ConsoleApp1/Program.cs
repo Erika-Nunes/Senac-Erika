dafc655f96f6bef9a90fa6b22be987d5a3ea3d33
Erika-Nunes <erikac0211@outlook.com>
2024-08-30 21:23:49 -0300

    Aula 11 - 30/08/2024
    Questão Restaurante

---
+++ Aula30-08/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Aula 11- 30/08/2024
+
+/*
+Questao 
+Classe Cliente 
+atributos: (Pessoas ista )
+- nome
+- telefone 
+
+classe Restaurante
+atributos:
+-fila
+
+funções:
+- AdicionarFila(nome,telefone)
+-Remover Fila (NOME)
+-Limpar Fila(NOME)
+-Imprimir Fila
+
+*/
+Restaurantes restaurante = new Restaurantes();
+restaurante.AdicionarCliente("A", "1");
+restaurante.AdicionarCliente("B", "2");
+restaurante.AdicionarCliente("C", "3");
+restaurante.AdicionarCliente("D", "4");
+
+restaurante.ImprimirFila();
+
+restaurante.RemoverCliente();
+
+restaurante.ImprimirFila();
+
+//restaurante.LimparFila();
---
+++ Aula30-08/ConsoleApp1/Restaurante.cs
@@ -0,0 +1,58 @@
+class Cliente {
+    private string nome;
+    private string telefone;
+
+    public Cliente(string nome, string telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public string GetNome() {
+        return nome;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome: {nome} | Telefone: {telefone}";
+    }
+} 
+
+class Restaurantes {
+    private List<Cliente> fila = new List<Cliente>();
+
+    public void AdicionarCliente(string nome, string telefone) {
+
+       Cliente cliente = new Cliente(nome, telefone);
+        // lista.Add(pessoa);
+
+        fila.Add(cliente);
+    }
+    public void RemoverCliente() {
+        if (fila.Count == 0){
+            return;
+        }
+        fila.RemoveAt(0);
+    }
+    public void LimparFila(string nome){
+        //fila = new List<Cliente>(); //eliminaria a lista antiga, pois está criando uma em cima
+        fila.Clear();
+        //for (int i = 0; i < fila.Count; i++)
+       //{
+           // fila.RemoveAt(i);
+        //}
+    }
+    public void ImprimirFila() {
+        //foreach (Cliente cliente in fila)
+       // {
+           // Console.WriteLine(fila.ToString());
+        //}
+    
+        for (int i = 0; i < fila.Count; i++)
+        {
+        Console.WriteLine($"{i} - {fila[i].ToString()}");
+        }
+    
+    
+    }
+
+}

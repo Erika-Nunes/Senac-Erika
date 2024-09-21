fac9b5615fe89859ffd5cac994cfc16c83742fe3
Erika-Nunes <erikac0211@outlook.com>
2024-09-20 21:13:43 -0300

    Projeto Maui-Blazor
    login

---
+++ MAUI/MauiBlazor/Models/LoginForms.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MauiBlazor.Models;
+
+public class LoginForms
+{
+    public string Username{ get; set;}
+    public string Email{ get; set;}
+    public string Password{ get; set;}
+}
